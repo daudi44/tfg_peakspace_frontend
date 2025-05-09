e8a681e839d850e25c4c3325b43e3661727367d7
Daniel Audí Bielsa <daniaudibielsa@gmail.com>
2025-05-09 18:09:08 +0200

    dashboard is looking cool

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,26 @@
   <div class="header">
     <img src="/logo3.svg" alt="Logo" style="height: 160px;margin: 0px 20%" />
     <div>
-      <button @click="navigateTo('/dashboard')">Dashboard</button>
-      <button @click="navigateTo('/productivity')">Productivity</button>
-      <button @click="navigateTo('/economy')">Economy</button>
+      <button :class="{ active: isActive('/dashboard') }" @click="navigateTo('/dashboard')">
+        <img :src="isActive('/dashboard') ? '/dashboard-active.svg' : '/dashboard.svg'" alt="Dashboard" />
+        Dashboard
+      </button>
+      <button :class="{ active: isActive('/productivity') }" @click="navigateTo('/productivity')">
+        <img :src="isActive('/productivity') ? '/productivity-active.svg' : '/productivity.svg'" alt="Productivity" />
+        Productivity
+      </button>
+      <button :class="{ active: isActive('/economy') }" @click="navigateTo('/economy')">
+        <img :src="isActive('/economy') ? '/economy-active.svg' : '/economy.svg'" alt="Economy" />
+        Economy
+      </button>
     </div>
     <div>
-      <button @click="navigateTo('/profile')">Profile</button>
-      <button @click="logout">Logout</button>
+      <button :class="{ active: isActive('/profile') }" @click="navigateTo('/profile')">
+        <img :src="isActive('/profile') ? '/user-active.svg' : '/user.svg'" alt="Profile" />
+      </button>
+      <button @click="logout">
+        <img src="/logout.svg" alt="Logout" />
+      </button>
     </div>
   </div>
 </template>
@@ -35,7 +48,10 @@ export default {
       userStore.logout()
 
       this.$router.push('/login')
-    }
+    },
+    isActive(path) {
+      return this.$route.path === path
+    },
   }
 }
 </script>
@@ -50,5 +66,38 @@ export default {
   height: 90px;
   display: flex;
   z-index: 1001;
+}
+
+.header div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.header button {
+  color: white;
+  border: none;
+  background-color: #545454;
+  margin: 0 10px;
+  cursor: pointer;
+  font-size: 20px;
+  border-radius: 8px;
+  box-shadow: none;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header button img {
+  height: 60px;
+  stroke: #7E4DF6;
+}
+
+button.active  {
+  background-color: #2C2C2C;
+  color: #7E4DF6;
 }
 </style>

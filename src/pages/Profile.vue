8efb55b5ff1d1400fda3f5cb6a4a620be81d4493
Daniel Audí Bielsa <daniaudibielsa@gmail.com>
2025-05-09 19:38:59 +0200

    profile structure created

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,8 +1,68 @@
 <template>
   <div style="margin-top: 180px;">
-    <h1 class="text-2xl font-bold">Your account details</h1>
+    <div class="profile-container">
+      <h1>Your account details</h1>  
+      <div class="profile-row">
+        <div class="profile-item">
+          <label for="Name">Name</label>
+          <input type="text" name="Name" id="Name">
+        </div>
+        <div class="profile-item">
+          <label for="Email">Email</label>
+          <input type="email" name="Email" id="Email">
+        </div>
+      </div>
+      <div class="profile-row">
+        <div class="profile-item">
+          <label for="Password">Password</label>
+          <input type="password" name="Password" id="Password">
+        </div>
+        <div class="profile-item">
+          <label for="ConfirmPassword">Confirm Password</label>
+          <input type="password" name="ConfirmPassword" id="ConfirmPassword">
+        </div>
+      </div>
+      <div style="display: flex; justify-content: flex-end;">
+        <button style="background-color: #5438DC; color: white; font-size: 16px; font-weight: bold; padding: 10px 20px; border-radius: 8px;">Update</button>
+      </div>
+    </div>
+    <button style="background-color: #FF4F4D; color: white; font-size: 16px; font-weight: bold; padding: 10px 20px; border-radius: 8px; justify-content: center; align-items: center; display: flex; gap: 10px; margin: 10px auto;">Delete your account <img src="../assets/trash.svg" alt="Trash" style="height: 22px;"></button>
   </div>
 </template>
 
-<script setup>
+<script>
 </script>
+
+<style scoped>
+.profile-container {
+    background-color: #D9D9D9;
+    border-radius: 16px;
+    padding: 20px;
+    width: 50%;
+    margin: 0 auto;
+}
+
+.profile-container label {
+    font-weight: bold;
+}
+
+.profile-container input {
+    border-radius: 4px;
+    border: none;
+    padding: 10px;
+    width: 100%;
+}
+
+.profile-row {
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
+    margin-bottom: 20px;
+    justify-content: center;
+}
+
+.profile-item {
+    display: flex;
+    flex-direction: column;
+}
+</style>
